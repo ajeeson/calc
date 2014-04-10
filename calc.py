01877a1914471e63d42a9722edacb056303d5c74
ajeeson <ajeeson@unimelb.edu.au>
2014-04-10 15:46:51 +1000

    New folder with oldcalc

---
+++ calc.py
@@ -0,0 +1,28 @@
+"""A simple code to calculate"""
+#!/usr/bin/python
+# Filename:calc.py
+
+#A=int(raw_input("Enter A:"))
+#B=int(raw_input("Enter B:"))
+#c = A + B 
+#print c
+
+# Now  we square C
+#print c*c
+
+import sys
+
+if __name__=='__main__':
+	command = sys.argv[1]
+	nums = [int[a] for a in sys.argv[2:]]
+	if command == 'add':
+		print sum(nums)
+	elif command == 'multiply':
+		print reduce(lambda x, y: x * y,nums)
+	else:
+		print 'Command not found' 
+	
+
+
+
+# random comment
